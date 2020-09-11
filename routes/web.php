1baa5068de5b567fc9595e8545e48e963c2938ca
oribanda <oribanda2@gmail.com>
2020-09-11 17:18:27 +0900

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::resource('home', 'HomeController');
+
+//下記を追記
+//画像をアップロードするページ
+Route::get('/upload', 'ImageController@input');
+//画像を保存したり画像名をDBに格納する部分
+Route::post('/upload', 'ImageController@upload');
+//保存した画像を表示するページ
+Route::get('/output', 'ImageController@output');
+//上記までを追記
