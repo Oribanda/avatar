4d5975a577d39e1edd29db7a280a74922286cb73
oribanda <oribanda2@gmail.com>
2020-09-11 17:54:08 +0900

    4th commit

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $table = 'image';
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('api/ver', 'ImageController@index');
+Route::get('api/image', 'ImageController@index');
